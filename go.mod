a7f3443d99a9cbecfb7c241a0713df3ec31b7958
Your Name <you@example.com>
2023-12-07 06:31:23 -0500

    rework for "fit" to be centered maximized within the space

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/disintegration/imaging v1.6.2
 
-require golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"flag"
+	"image"
+	"image/color"
 	"image/jpeg"
 	"image/png"
 	"log"
+	"math"
 	"os"
 
 	"github.com/disintegration/imaging"
+	"github.com/nfnt/resize"
 )
 
 func main() {
 	var width int
 	var height int
 	var filename string
+	var fit bool
 	var output string
 
 	flag.IntVar(&width, "width", 320, "Width of output image")
 	flag.IntVar(&height, "height", 240, "Height of output image")
 	flag.StringVar(&filename, "filename", "", "Input image filename")
 	flag.StringVar(&output, "output", "", "Output filename")
+	flag.BoolVar(&fit, "fit", false, "Resize image so the entire thing fits within the specified dimensions")
 	flag.Parse()
 
 	in, err := os.Open(filename)
@@ -41,13 +47,37 @@ func main() {
 		}
 	}
 
-	dstImageFill := imaging.Fill(d, width, height, imaging.Center, imaging.Lanczos)
+	var dst *image.NRGBA
+
+	if fit {
+		bounds := d.Bounds()
+
+		imageaspect := bounds.Dy() / bounds.Dx()
+		fitaspect := height / width
+		if imageaspect > fitaspect {
+			d = resize.Resize(uint(width), 0, d, resize.Lanczos3)
+		} else {
+			d = resize.Resize(0, uint(height), d, resize.Lanczos3)
+
+		}
+		bounds = d.Bounds()
+		gap := height - bounds.Dy()
+		drop := int(math.Round(float64(gap) / 2))
+		gap = width - bounds.Dx()
+		slide := int(math.Round(float64(gap) / 2))
+
+		blank := imaging.New(width, height, color.Black)
+
+		d = imaging.Paste(blank, d, image.Pt(slide, drop))
+	}
+
+	dst = imaging.Fill(d, width, height, imaging.Center, imaging.Lanczos)
 	out, err := os.Create(output)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	png.Encode(out, dstImageFill)
+	png.Encode(out, dst)
 	out.Close()
 
 }
